760807d896aa95550344c098dcb42e0e088958bb
afrojuergi <afrojuergi@gmail.com>
2022-02-24 15:25:03 +0100

    set up a suare of squares with javascript

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("it works!");
+
+const CONTAINER = document.querySelector(".container");
+const WIDTH = parseInt(window.getComputedStyle(CONTAINER).width);
+
+const count = 16;
+
+const squares = [];
+
+for (let i = 0; i < count; i++) {
+    for (let j = 0; j < count; j++) {
+        squares[i] = document.createElement("div");
+        squares[i].setAttribute("class", "square");
+        squares[i].style.width = `${WIDTH / count}px`;
+        squares[i].style.height = `${WIDTH / count}px`;
+        CONTAINER.appendChild(squares[i]);
+    }
+}
+console.log(squares);
+console.log(`${WIDTH / 16}px`);
