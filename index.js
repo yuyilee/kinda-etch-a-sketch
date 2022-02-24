88afad45eaa18d27ffe1e50265683669242e325c
afrojuergi <afrojuergi@gmail.com>
2022-02-24 15:56:10 +0100

    set up MVP of sketch-up

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ console.log("it works!");
 const CONTAINER = document.querySelector(".container");
 const WIDTH = parseInt(window.getComputedStyle(CONTAINER).width);
 
-const count = 16;
+const count = 50;
 
 const squares = [];
+
+function hover(e) {
+    e.target.classList.add("black");
+}
 
 for (let i = 0; i < count; i++) {
     for (let j = 0; j < count; j++) {
@@ -16,5 +20,7 @@ for (let i = 0; i < count; i++) {
         CONTAINER.appendChild(squares[i]);
     }
 }
-console.log(squares);
+const square = document.querySelectorAll(".square");
+square.forEach(square => square.addEventListener("mouseover", hover));
+console.log(square);
 console.log(`${WIDTH / 16}px`);
